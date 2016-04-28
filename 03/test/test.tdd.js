7f5395b980b620fce12ff7ee8adce322bcdaab9d
Miguel Isasmendi <miguel.isasmendi@globant.com>
2016-04-28 12:38:20 -0300

    fix para correr los tests

--- 03/test/test.tdd.js
+++ 03/test/test.tdd.js
@@ -4,47 +4,44 @@ var CourseModule = require(__dirname+'/../../01/modules/Course.js');
 
 var assert = require('chai').assert;
 
-var course = null;
+
 
 suite('CourseModule', function(){
-  setup(function(){
-  });
+	test('Course class', function(){
+		assert(CourseModule.Course, 'The Course class is not imported!!!!');
+	});
 
- assert(CourseModule.Course, 'The Course class is not imported!!!!');
+	suite('Course function existence', function(){
+		var Course = null;
+		setup(function(){
+			Course = CourseModule.Course;
+		});
 
-
-suite('Course', function(){
-  setup(function(){
-    course = new CourseModule.Course();
-  });
-
-  suite('Basic Initialization', function(){
-
-
-
-assert(Array.isArray([]), 'empty arrays are arrays'
-
-    test('should return null on initialization', function(){
-      assert.equal(null, course.name);
-    };
-    
-	test('should returnnull on initialization', function(){
-      assert.equal(0, course.students.len);
-    };
-
-    test('should returnnull on initialization', function(){
-      assert.equal(0, course.students.len);
-    };
-
-    test('should returnnull on initialization', function(){
-      assert.equal(null, course.name);
-    };
-    );
+		test('checking expected functions existence', function(){
+			assert.notEqual(null, Course.prototype.setTeacher,'#setTeacher has to exists in the definition of the Teacher class');
+			assert.notEqual(null, Course.prototype.addStudent,'#addStudent has to exists in the definition of the Teacher class');
+			assert.notEqual(null, Course.prototype.removeStudent,'#removeStudent has to exists in the definition of the Teacher class');
+		});
+		
+	});
 
 
-	this.name = newName;
-	this.students = [];
-	this.minimum_avg_grade = 0;
+	suite('Course empty initialization', function(){
+		var course = null;
 
-  });
+		setup(function(){
+			course = new CourseModule.Course();
+		});
+
+		test('variables status', function(){
+			assert.equal(null, course.name,'property name should return null on initialization!!!');
+			assert.notEqual(null, course.students,'property students should not return null on initialization!!!');
+
+			assert.equal([], course.students.length,'property students should return an empty Array on initialization!!!');
+			
+			assert.equal(0, course.minimum_avg_grade,'property minimum_avg_grade should return an empty Array on initialization!!!');
+		});
+		
+	});
+
 });
